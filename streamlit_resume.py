7a59b6b78c56fa06382418766fa7d743e0789f5f
Aditya Kadrekar <43070287+adityakadrekar16@users.noreply.github.com>
2020-12-01 16:10:17 -0800

    Update streamlit_resume.py

--- streamlit_resume.py
+++ streamlit_resume.py
@@ -1,6 +1,13 @@
 import streamlit as st
 
-st.text('Aditya Kadrekar')
+# resume title
+st.title('Aditya Kadrekar')
+st.markdown('https://github.com/adityakadrekar16')
 
 # choose filter
 st.sidebar.title('Choose ...')
+
+# https://github.com/streamlit/streamlit/issues/1088
+# base64_pdf = base64.b64encode(pdf_file.read()).decode('utf-8')
+# pdf_display = f'<embed src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf">' 
+# st.markdown(pdf_display, unsafe_allow_html=True)
